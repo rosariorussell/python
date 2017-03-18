ef0337c5cefd0da926985029498326bded3439da
Russell Rosario <rosariorussell@gmail.com>
2017-03-18 01:36:48 -0400

    first python

---
+++ 1.py
@@ -0,0 +1,13 @@
+# import requests
+#
+# res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+# print(res.status_code)
+#
+#
+# def say(param):
+#     pass
+
+
+var = 'test'
+print("This is a {}".format(var))
+
